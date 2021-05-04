f2a64f832832a7e54d7966e949da42bb63cf1dbf
Marcelo <carbono.marcelo@gmail.com>
2021-05-03 23:00:31 -0300

    refresh token ok

--- src/modules/users/useCases/RefreshToken/RefreshTokenUseCase.ts
+++ src/modules/users/useCases/RefreshToken/RefreshTokenUseCase.ts
@@ -1,7 +1,8 @@
 import auth from "@config/auth";
 import { IUsersTokensRepository } from "@modules/users/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/dateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { verify } from "jsonwebtoken";
+import { sign, verify } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
 interface IResponse {
@@ -20,23 +21,46 @@ class RefreshTokenUseCase {
     constructor(
         @inject("UsersTokensRepository")
         private usersTokensRepository: IUsersTokensRepository,
+
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
     ){}
 
     async execute(token: string): Promise<IResponse>{
         const { email, sub } = verify(token, auth.refresh_token_secret) as IPayload;
 
         const user_id = sub
-
-        const refresh_token_exists = await this.usersTokensRepository.findByIdAndToken(user_id, token)
-
-        if(!refresh_token_exists){
+       
+        const refreshTokenUser = await this.usersTokensRepository.findByIdAndToken(user_id, token)
+        
+        if(!refreshTokenUser){
             throw new AppError("Refresh Token does not exists!")
         }
+        
+        await this.usersTokensRepository.deleteById(refreshTokenUser.id)
+    
+        const expires_date = this.dateProvider.addDays(auth.expires_refresh_token_days)
 
-        await this.usersTokensRepository.deleteById(token)
+        const refresh_token = sign({ email }, auth.refresh_token_secret, {
+            subject: user_id,
+            expiresIn: auth.expires_in_refresh_token
+        })
+        
+        await this.usersTokensRepository.create({
+            refresh_token,
+            expires_date,
+            user_id
+        })
 
+        const newToken = sign({}, auth.expires_in_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
 
-        return
+        return {
+            token: newToken,
+            refresh_token
+        }
     }
 }
 

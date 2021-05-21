7d89eb27edc0de7b2dd96947991dd7cf1ea20dad
Marcelo Carbono <carbono.marcelo@gmail.com>
2021-05-21 12:19:13 -0300

    api docs ok for now, refactoring in refused mentoring

--- src/modules/mentoring/useCases/DeleteMentoring/DeleteMentoringController.ts
+++ src/modules/mentoring/useCases/DeleteMentoring/DeleteMentoringController.ts
@@ -5,12 +5,12 @@ import { DeleteMentoringUseCase } from './DeleteMentoringUseCase';
 class DeleteMentoringController {
     async handle(request: Request, response: Response):Promise<Response>{
         const { mentoring_id } = request.params;
-        const { id: mentor_id } = request.user;
+        const { is_mentor } = request.user;
         const { refused_info } = request.body;
 
         const deleteMentoringUseCase = container.resolve(DeleteMentoringUseCase)
 
-        await deleteMentoringUseCase.execute({mentor_id, mentoring_id, refused_info})
+        await deleteMentoringUseCase.execute({ mentoring_id, refused_info, is_mentor})
         
         return response.status(204).json({ message: "Mentoring refused"})
     }
--- src/modules/mentoring/useCases/DeleteMentoring/DeleteMentoringUseCase.ts
+++ src/modules/mentoring/useCases/DeleteMentoring/DeleteMentoringUseCase.ts
@@ -7,13 +7,12 @@ import { resolve } from 'path';
 
 interface IRequest {
     mentoring_id: string;
-    mentor_id: string;
     refused_info: string;
+    is_mentor: boolean;
 }
 
 @injectable()
 class DeleteMentoringUseCase {
-
     constructor(
         @inject("MentoringRepository")
         private mentoringRepository: IMentoringRepository,
@@ -25,50 +24,86 @@ class DeleteMentoringUseCase {
         private mailProvider: IMailProvider,
     ){}
 
-    async execute({mentor_id, mentoring_id, refused_info}: IRequest){
-        const mentorAndMentoring = await this.mentoringRepository.findMentoringAndMentor(mentor_id, mentoring_id);
+    async execute({ mentoring_id, refused_info, is_mentor }: IRequest){
+        const mentoring = await this.mentoringRepository.findMentoringById(mentoring_id);
 
-        const { start_date } = mentorAndMentoring.mentors_availability;
+        if(!mentoring){
+            throw new AppError("Mentoring not found", 404)
+        }
 
-        const templateFile = resolve(__dirname, '..', '..', 'views', 'emails', 'deletedMentoring.hbs')
-        
+        const { start_date } = mentoring.mentors_availability;
+
+        if(mentoring.isDone){
+            throw new AppError("Mentoring was already done")
+        }
+
+        if(mentoring.refused){
+            throw new AppError("Mentoring was already cancelled")
+        }
+
         if(this.dateProvider.dateNow() > start_date){
             throw new AppError("It's not possible to cancel a mentoring after his initial time");
         }
 
-        if(mentorAndMentoring.isDone){
-            throw new AppError("Mentoring was already done")
-        }
-
-        if(mentorAndMentoring.refused){
-            throw new AppError("Mentoring was already cancelled")
-        }
-        
-        const { email, first_name: name_user } = mentorAndMentoring.user
-        const { first_name: name_mentor } = mentorAndMentoring.mentor
-
         const mentoringDate = this.dateProvider.convertTimestampToDate(start_date)
         const mentoringTime = this.dateProvider.convertTimestampToHoursMinutes(start_date)
 
-        mentorAndMentoring.refused = true;
-        mentorAndMentoring.refused_info = refused_info;
+        mentoring.refused = true;
+        mentoring.refused_info = refused_info;
 
-        await this.mentoringRepository.create(mentorAndMentoring);
+        await this.mentoringRepository.create(mentoring);
 
-        const variables = {
+        let { email, first_name: name_user } = mentoring.user
+        let { first_name: name_mentor } = mentoring.mentor
+
+        let templateFile = resolve(__dirname, '..', '..', 'views', 'emails', 'deletedMentoringByMentor.hbs')
+
+        let variables = {
             name_user,
             name_mentor,
             mentoringDate,
             mentoringTime,
-            refused_info: mentorAndMentoring.refused_info
+            refused_info: mentoring.refused_info
         }
 
-        await this.mailProvider.sendMail(
-            email,
-            "Cancelamento da mentoria",
-            variables,
-            templateFile
-        )
+        if(is_mentor) {
+            await this.mailProvider.sendMail(
+                email,
+                "Cancelamento da mentoria",
+                variables,
+                templateFile
+            )
+
+            return
+
+        } else {
+            email = mentoring.mentor.email;
+            name_mentor = mentoring.mentor.first_name;
+            name_user = mentoring.user.first_name;
+
+            variables = {
+                name_user,
+                name_mentor,
+                mentoringDate,
+                mentoringTime,
+                refused_info: mentoring.refused_info
+            }
+
+            templateFile = resolve(__dirname, '..', '..', 'views', 'emails', 'deletedMentoringByUser.hbs')
+
+            await this.mailProvider.sendMail(
+                email,
+                "Cancelamento da mentoria",
+                variables,
+                templateFile
+            )
+
+            return
+        }
+
+        
+
+      
 
         return
     }
